773519c2d40b76875e1dbcbc2b2de0da708a7a1a
Maurice Yong <mauriceyong@Maurices-MacBook-Pro.local>
2018-04-19 17:37:45 +1000

    Reset users and profiles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,8 +5,9 @@ module ApplicationHelper
 
     def show_all_google_map profiles_all
         main_map = "http://maps.googleapis.com/maps/api/staticmap?size=600x300"
+        count = 0
         profiles_all.each do |p|
-            main_map += "&markers=#{p.latitude},#{p.longitude}"
+            main_map += "&markers=label:#{count+=1}%7C#{p.latitude},#{p.longitude}"
         end
         main_map += "&key=AIzaSyD964KFUMyHkWZVdc4RkUsVy_3jLWjJCbU"
         return image_tag main_map
