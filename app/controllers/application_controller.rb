d74e1ad7564fdc77b32a2db86e44007f26c5689a
Maurice Yong <mauriceyong@Maurices-MacBook-Pro.local>
2018-04-19 10:52:04 +1000

    Update Google API key and Layout of Profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,7 +6,13 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for user
     # return path eg. 'profile/edit'
-    profile_edit_path if user.profile.nil?
+    # profile_edit_path if user.profile.nil?
+
+    if user.profile.nil?
+      profile_edit_path
+    else
+      pages_home_path
+    end
   end
 
 end
